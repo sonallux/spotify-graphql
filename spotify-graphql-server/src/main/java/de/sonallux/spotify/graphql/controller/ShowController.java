bb2edca1f4fc220f7f3f847e3e2b7dc98723ba5c
sonallux <13821543+sonallux@users.noreply.github.com>
2022-05-03 20:21:34 +0200

    Add show's episode endpoint

--- spotify-graphql-server/src/main/java/de/sonallux/spotify/graphql/controller/ShowController.java
+++ spotify-graphql-server/src/main/java/de/sonallux/spotify/graphql/controller/ShowController.java
@@ -3,6 +3,7 @@ package de.sonallux.spotify.graphql.controller;
 import org.dataloader.DataLoader;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Mono;
@@ -24,5 +25,10 @@ public class ShowController extends BaseController {
                                            DataLoader<String, Map<String, Object>> showLoader
     ) {
         return loadMany(ids, uris, "show", showLoader);
+    }
+
+    @SchemaMapping(typeName = "Show")
+    Mono<Map<String, Object>> episodes(Map<String, Object> show, @Argument Map<String, Object> arguments, DataLoader<String, Map<String, Object>> rawLoader) {
+        return loadPagingObject(show, arguments, "episodes", rawLoader);
     }
 }
